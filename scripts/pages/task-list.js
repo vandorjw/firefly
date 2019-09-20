9be0fbb93a955d00d71f8bf9f26f296d5f6dba5c
Joost Vandorp <joostvandorp@gmail.com>
2019-09-20 16:44:24 -0400

    Feature/tasks (#7)
    * stop the accidental creating of task when user is not signed in
    
    * minor key fix
    
    * It is not pretty, but tasks are loading properly.
    
    * Adding tasks to the list :)

--- scripts/pages/task-list.js
+++ scripts/pages/task-list.js
@@ -1,41 +1,17 @@
 const TaskList = Vue.component('TaskList', {
-    data () {
-        return {
-            taskIds: [] 
-        }
-    },
     computed: {
-        user: function () {
-            return this.$store.getters.user
+        tasks: function () {
+            return this.$store.getters.tasks
         }
-    },
-    methods: {
-        getTaskIds: function() {
-            var self = this;
-            firebase.firestore().collection('users').doc(this.user.uid).collection("tasks").get().then(querySnapshot => {
-                querySnapshot.forEach(function(doc) {
-                    self.taskIds.push(doc.id);
-                });
-            })
-        }
-    },
-    created() {
-        if(this.user){
-            this.getTaskIds();
-        }
-        
     },
     updated() {
-        dragula([document.querySelector('#left'), document.querySelector('#right')]);
+        dragula([document.getElementById('taskList')]);
     },
     template: `
     <div class="grid-x grid-margin-x small-up-2 medium-up-3">
-    <add-task></add-task>
-        <div class="small-6" id="left">
-            <task-card v-for="id in taskIds" :taskId='id'></task-card>
-        </div>
-        <div class="small-6" id="right">
-            <task-card v-for="id in taskIds" :taskId='id'></task-card>
+        <div class="small-6" id="taskList">
+            <add-task></add-task>
+            <task-card v-for="(task, index) in tasks" v-bind:key="index" :task='task'></task-card>
         </div>
     </div>`
-  })
+})
